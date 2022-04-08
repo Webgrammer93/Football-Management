fb952b40b203e4821a332fa9e34ea1589fa40c3b
acorella <acorella@tradestation.com>
2022-04-07 20:48:37 -0600

    first commit

---
+++ force-app/main/default/lwc/addStadium/addStadium.js
@@ -0,0 +1,37 @@
+import { LightningElement, api } from 'lwc';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+
+import OBJECT from '@salesforce/schema/Stadium_Register__c';
+import SR_ROLE_FIELD from '@salesforce/schema/Stadium_Register__c.Stadium_Role__c';
+import SR_STADIUM_LK_FIELD from '@salesforce/schema/Stadium_Register__c.Stadium__c';
+import SR_FC_LK_FIELD from '@salesforce/schema/Stadium_Register__c.Football_Club__c';
+
+export default class AddStadium extends LightningElement {
+
+    @api recordId
+
+    object = OBJECT;
+    fields = [SR_FC_LK_FIELD, SR_STADIUM_LK_FIELD, SR_ROLE_FIELD];
+
+    onSuccess(event) {
+        const evt = new ShowToastEvent({
+            title: 'Stadium Registry Created',
+            message: 'Record ID: ' + event.detail,
+            variant: 'success',
+        });
+        this.dispatchEvent(evt);
+        const editForm = this.template.querySelector('lightning-record-form');
+        editForm.recordId = null;
+    }
+
+    onError(event){
+        const evt = new ShowToastEvent({
+            title: 'Error on Create',
+            message: event,
+            variant: 'error',
+        });
+        this.dispatchEvent(evt);
+    }
+
+
+}
